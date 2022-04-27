496fa7d9cc689a67b994d3914131827a72e37710
Jhonata Luiz <Jhonataluiz@hotmail.com>
2022-04-27 00:12:40 -0300

    GET /orders

---
+++ backend/src/main/java/com/luizjhonata/delivery/controllers/OrderController.java
@@ -0,0 +1,27 @@
+package com.luizjhonata.delivery.controllers;
+
+import com.luizjhonata.delivery.dto.OrderDTO;
+import com.luizjhonata.delivery.entities.Order;
+import com.luizjhonata.delivery.services.OrderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "orders")
+public class OrderController {
+
+    @Autowired
+    private OrderService service;
+
+    @GetMapping
+    public ResponseEntity<List<OrderDTO>> findAll(){
+        List<OrderDTO> list = service.findAll();
+        return ResponseEntity.ok().body(list);
+    }
+
+}
